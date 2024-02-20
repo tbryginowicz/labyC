8e5b61febea375b69c6d6ef48afbd74e42f4c45e
Tomasz Bryginowicz <39021418+tbryginowicz@users.noreply.github.com>
2024-02-20 09:10:38 +0100

    Zadanie pierwsze

---
+++ zadanie1.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    char imie[80];
+    printf("Podaj swoje imie: ");
+    scanf("%s", imie);
+    printf("Masz na imie %s",imie);
+
+    return 0;
+}
