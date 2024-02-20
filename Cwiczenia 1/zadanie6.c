3c2f9438dc637695cfa1c392b3a4e897b29b55cf
Tomasz Bryginowicz <39021418+tbryginowicz@users.noreply.github.com>
2024-02-20 15:37:16 +0100

    Update zadanie6.c

--- Cwiczenia 1/zadanie6.c
+++ Cwiczenia 1/zadanie6.c
@@ -10,21 +10,41 @@ int main() {
     char i2[7]; //input gracza 2
 
     int isRunning = 1;
-    printf("Mozliwe komendy: papier, kamien, nozyce, wynik, stop");
+    printf("Dostepne polecenia: papier, kamien, nozyce, wynik, stop\n");
     while (isRunning == 1){
         printf("Ruch gracza 1: ");
-        scanf("%s",g1);
+        scanf("%s",i1);
         printf("Ruch gracza 2: ");
-        scanf("%s",g2);
-        switch (g1){
-            case "kamien":
-                /* code */
-                break;
-            case "papier":
-               /* code */
-               break;
+        scanf("%s",i2);
+        if (strcmp(i1, "kamien") == 0 && strcmp(i2, "nozyce") == 0) {
+            printf("Gracz 1 wygrywa!\n");
+            g1++;
+        } else if (strcmp(i1, "kamien") == 0 && strcmp(i2, "papier") == 0) {
+            printf("Gracz 2 wygrywa!\n");
+            g2++;
+        } else if (strcmp(i1, "papier") == 0 && strcmp(i2, "kamien") == 0) {
+            printf("Gracz 1 wygrywa!\n");
+            g1++;
+        } else if (strcmp(i1, "papier") == 0 && strcmp(i2, "nozyce") == 0) {
+            printf("Gracz 2 wygrywa!\n");
+            g2++;
+        } else if (strcmp(i1, "nozyce") == 0 && strcmp(i2, "papier") == 0) {
+            printf("Gracz 1 wygrywa!\n");
+            g1++;
+        } else if (strcmp(i1, "nozyce") == 0 && strcmp(i2, "kamien") == 0) {
+            printf("Gracz 2 wygrywa!\n");
+            g2++;
+        } else if (strcmp(i1, "wynik") == 0 || strcmp(i2, "wynik") == 0) {
+            printf("Gracz 1: %d punktow, Gracz 2: %d punktow\n", g1, g2);
+        } else if (strcmp(i1, "stop") == 0 || strcmp(i2, "stop") == 0) {
+            printf("Gracz 1: %d punktow, Gracz 2: %d punktow\nKONIEC GRY!", g1, g2);
+            isRunning = 0;
+        } else if (strcmp(i1, i2) == 0) {
+            printf("Remis!\n");
+        } else {
+            printf("Niepoprawny ruch!\n");
         }
-
+        
     } 
 
     return 0;
