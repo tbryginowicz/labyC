ec5cba881a2757f059f2eaeffbf74986a4ac2544
Tomasz Bryginowicz <39021418+tbryginowicz@users.noreply.github.com>
2024-02-20 09:54:06 +0100

    Update zadanie3.c

--- Cwiczenia 1/zadanie3.c
+++ Cwiczenia 1/zadanie3.c
@@ -1,19 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int silnia(int tab[], int size);
+int silnia(int liczba);
 
 int main() {
-    int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; 
-    printf("%d",silnia(array, sizeof(array)/sizeof(array[0])));
-
+    int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    for (int i = 0; i < sizeof(array)/sizeof(array[0]); i++){
+        printf("%d \n",silnia(array[i]));
+    }
+    
     return 0;
 }
 
-int silnia(int tab[], int size){
+int silnia(int liczba){
     int wynik = 1;
-    for(int i = 0; i < size; i++){
-        wynik *=tab[i];
+    for(int i = 1; i <= liczba; i++){
+        wynik*=i;
     }
     return wynik;
 }
